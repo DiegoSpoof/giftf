57125f08bc8da9264d55a1cf1215d8a268a8433a
DiegoSpoof <108324504+DiegoSpoof@users.noreply.github.com>
2022-06-28 01:51:39 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Inicio } from './Inicio';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <Inicio />
-  </>
-);
